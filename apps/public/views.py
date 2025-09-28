61f2fd956071c9865721cd8d22f11bb903ca4adf
justine-andrie-v-abella <justineandrieabella@gmail.com>
2025-09-28 23:25:51 +0800

    my last code from the tutorial

---
+++ apps/public/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    print(request.user)
+    return render(request, "index.html")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "about.html")
+
+
+def contact(request: HttpRequest) -> HttpResponse:
+
+    return render(request, "contact.html")
--- test_banker/urls.py
+++ test_banker/urls.py
@@ -16,22 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
-    path("contact/", views.contact, name="contact"),
+    path("", include("apps.public.urls")),
     path(
-        "accounts/login/",
-        auth_views.LoginView.as_view(template_name="accounts/login.html"),
-        name="login",
+        "accounts/",include("apps.accounts.urls")
     ),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path(
-        "accounts/profile/", views.ProfileView.as_view(), name="profile"
-    ),  # Redirect to index after login
 ]
